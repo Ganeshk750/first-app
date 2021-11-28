34bd37b8dec11f6f7ec7d22b2415634d8922b33f
Ganeshk750 <ganesh9ot@gmail.com>
2021-11-28 22:49:16 +0530

    added all file

---
+++ src/main/java/com/ganesh/TestController.java
@@ -0,0 +1,16 @@
+package com.ganesh;
+
+public class TestController {
+	
+	public int add(int i, int j){
+		return i+j;
+	}
+	public int multiply(int i, int j){
+		return i*j;
+	}
+	
+	public int sub(int i, int j){
+		return i-j;
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+	<h1 style="color: skyblue; font-size: 50px;" align="center"> Wel Come To First App</h1>
+</body>
+</html>
